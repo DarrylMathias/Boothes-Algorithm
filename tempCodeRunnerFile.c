cec8b36b70e25ab077fa696633fa8d2fc8e24cc8
Darryl Mathias <mathiasndarryl7@gmail.com>
2025-07-18 00:31:06 +0530

    Changes

--- tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-printf("Position %d: a=%d, b=%d, carry=%d\n", i, a, b, carry);
-        // printf("case000=%d, case001=%d, case010=%d, case011=%d, case110=%d, case111=%d\n",
-        //        case000, case001, case010, case011, case110, case111);
